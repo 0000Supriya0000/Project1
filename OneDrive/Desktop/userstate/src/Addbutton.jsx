ef03d6fba848f664a8e627a104ed37ee3d4d3e59
Supriya <0000supriya0000@gmail.com>
2023-03-11 23:48:22 +0530

    Homework done

--- OneDrive/Desktop/userstate/src/Addbutton.jsx
+++ OneDrive/Desktop/userstate/src/Addbutton.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import {useState,React} from 'react'
 
-function Addbutton(props) {
-    return (
-        <div>
 
-            <p>hello world</p>
-            
-        </div>
-    );
+export default function Count() {
+    const [text , setText]= useState('')
+    const[list,setList]= useState([]);
+
+    function handleonChange(event){
+        setText(event.target.value)
+    }
+    function handleAdd(){
+        setList([...list,text]);
+        setText('');
+    }
+
+  return (
+    <>
+    <h1>bbbd</h1>
+    <input type='text' value={text} onChange={handleonChange}/>
+    <button onClick={handleAdd} style={{marginleft:'10px'}}>Add</button>
+     <ul>
+        {list.map((elem,index)=>{
+          return (
+            <li key={index}>{elem}</li>
+          )  
+        })}
+     </ul>
+     </>
+)
 }
-
-export default Addbutton;
